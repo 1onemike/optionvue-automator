7de31108655f94a33cf784c70568df7db45aefa8
Michael Osei <moaduamah@gmail.com>
2020-05-09 03:24:06 -0700

    new update

---
+++ Visual Studio Solution/OptionVue Automator/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace OptionVue_Automator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressDialog1 = new Ookii.Dialogs.WinForms.ProgressDialog(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selectBtn
+            // 
+            resources.ApplyResources(this.selectBtn, "selectBtn");
+            this.selectBtn.Name = "selectBtn";
+            this.selectBtn.UseVisualStyleBackColor = true;
+            this.selectBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Name = "label1";
+            // 
+            // startBtn
+            // 
+            resources.ApplyResources(this.startBtn, "startBtn");
+            this.startBtn.Name = "startBtn";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Name = "label3";
+            // 
+            // progressDialog1
+            // 
+            resources.ApplyResources(this.progressDialog1, "progressDialog1");
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Name = "label4";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.selectBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Ookii.Dialogs.WinForms.ProgressDialog progressDialog1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Visual Studio Solution/OptionVue Automator/Form1.cs
@@ -0,0 +1,346 @@
+﻿using Ookii.Dialogs.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OptionVue_Automator
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        String OutputPath;
+        String OutDir;
+        String SelectedFolder;
+        string[] Folder;
+        bool halt = false;
+      
+
+        Excel.Application oXL= new Microsoft.Office.Interop.Excel.Application();
+        Excel._Workbook oWB;
+        Excel._Worksheet oSheet;
+
+        Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+        Excel.Workbook xlWorkbook;
+        Excel._Worksheet xlWorksheet;
+        Excel.Range xlRange;
+
+        public string fsymbol { get; private set; }
+        public string fdate { get; private set; }
+        public string ftime { get; private set; }
+
+
+/////////////// MAIN PROG //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SelectFolder();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (halt) { return; }
+
+                else {
+
+                    if (startBtn.Text == "View Output")
+                    {
+                        Process.Start(SelectedFolder+"/Output");
+                        return;
+                    }
+
+
+                    startBtn.Enabled = false;
+                    label2.Visible = true;
+
+                    // CREATE OUTPUT FILE///////////////////////////////////////////////////////
+                    if (File.Exists(OutputPath))
+                    {
+                        LanguageUtils.IgnoreErrors(() => File.Delete(OutputPath));
+                    }
+                    else if (!Directory.Exists(OutDir))
+                    {
+                        Directory.CreateDirectory(OutDir);
+                    }
+
+                    // CHECK IF EXCEL IS INSTALLED/////////////////////////////////////////////////
+                    if (oXL == null)
+                    {
+                        MessageBox.Show("Excel is not installed!!");
+                        return;
+                    }
+                    // INSERT HEADER ROW///////////////////////////////////////////////////
+                    object misValue = System.Reflection.Missing.Value;
+                    oWB = oXL.Workbooks.Add(misValue);
+                    oSheet = (Excel.Worksheet)oWB.Worksheets.get_Item(1);
+                    oSheet.Cells[1, 1] = "SYMBOL";
+                    oSheet.Cells[1, 2] = "DATE";
+                    oSheet.Cells[1, 3] = "TIME";
+                    oSheet.Cells[1, 4] = "AVERAGE IV";
+
+
+                   // CALCULATE AVERAGE FROM FILES AND PUSH RESULTS TO OUTPUT FILE//////////////
+
+                    foreach (string excelFile in Folder)
+                    {
+                        double avg = calculateAVG(getMIDIV(excelFile));
+                        fsymbol = SplitFilename(excelFile, 1);
+                        fdate = SplitFilename(excelFile, 2);
+                        ftime = SplitFilename(excelFile, 3);
+
+                        xlRange = oSheet.UsedRange;
+                        int rowCount = xlRange.Rows.Count;
+
+                        oSheet.Cells[rowCount + 1, 1] = fsymbol;
+                        oSheet.Cells[rowCount + 1, 2] = fdate;
+                        oSheet.Cells[rowCount + 1, 3] = ftime;
+                        oSheet.Cells[rowCount + 1, 4] = avg;
+                    }
+                    LanguageUtils.IgnoreErrors(() => oWB.SaveAs(OutputPath));
+
+                    oXL.Quit();
+
+                    label3.Visible = true;
+                    startBtn.Enabled = true;
+                    startBtn.Text = "View Output";
+
+
+
+
+                }
+            
+         
+
+          
+        }
+
+
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        private void SelectFolder()
+        {
+            VistaFolderBrowserDialog dlg = new VistaFolderBrowserDialog();
+
+            dlg.SelectedPath = Properties.Settings.Default.StoreFolder;
+
+            dlg.ShowNewFolderButton = true;
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                SelectedFolder = dlg.SelectedPath;
+                OutputPath = SelectedFolder + "/Output/Output Average Implied Volatility.xlsx";
+                OutDir = SelectedFolder + "/Output/";
+
+                Folder = Directory.GetFiles(SelectedFolder, "*.xlsx");
+
+                if(Folder.Length == 0)
+                {
+                    label4.Visible = true;
+                    halt = true;
+                    return;
+
+                }else
+                {
+                    label4.Visible = false;
+
+                    label1.Visible = true;
+                    halt = false;
+                }
+
+                textBox1.Text = dlg.SelectedPath;
+            }
+
+        }
+
+        private void createOutputFile()
+        {
+            if (File.Exists(OutputPath))
+            {
+                LanguageUtils.IgnoreErrors(() => File.Delete(OutputPath));
+            }else if (!Directory.Exists(OutDir))
+            {
+                Directory.CreateDirectory(OutDir);
+            }
+            object misValue = System.Reflection.Missing.Value;
+
+            if (oXL == null)
+            {
+                MessageBox.Show("Excel is not installed!!");
+                return;
+            }
+
+            oWB = oXL.Workbooks.Add(misValue);
+            oSheet = (Excel.Worksheet)oWB.Worksheets.get_Item(1);
+            oSheet.Cells[1, 1] = "SYMBOL";
+            oSheet.Cells[1, 2] = "DATE";
+            oSheet.Cells[1, 3] = "TIME";
+            oSheet.Cells[1, 4] = "AVERAGE IV";
+
+            LanguageUtils.IgnoreErrors(() => oWB.SaveAs(OutputPath));
+
+            oXL.Quit();
+
+
+        }
+
+       
+
+        private List<double> getMIDIV(string fname)
+        {
+            List<double> MIDIVList = new List<double>();
+            xlWorkbook = xlApp.Workbooks.Open(fname);
+            xlWorksheet = xlWorkbook.Sheets[1];
+            xlRange = xlWorksheet.UsedRange;
+            int rowCount = xlRange.Rows.Count;
+
+            for (int i = 2; i <= rowCount; i++)
+            {
+                if (xlWorksheet.Cells[i, 8].value != null)
+                {
+                    MIDIVList.Add(xlWorksheet.Cells[i, 8].value);
+                }
+
+            }
+            xlWorkbook.Close();
+            xlApp.Quit();
+
+            return MIDIVList;
+        }
+
+        private double calculateAVG(List<double> list)
+        {
+            return list.Average();
+
+
+        }
+
+        private string SplitFilename(string fileinput, int choice)
+        {
+            string symbol = null;
+            string date = null;
+            string time = null;
+            string avg = null;
+
+            string fileName = Path.GetFileNameWithoutExtension(fileinput);
+            string trim = Regex.Replace(fileName, @"s", "");
+
+            
+            time = fileName.Substring(fileName.Length - 4);
+            int hh = Convert.ToInt32(time.Substring(0, 2));
+            int mm = Convert.ToInt32(time.Substring(2, 2));
+            if (hh > 12)
+            {
+                time = Convert.ToString((hh - 12)) + ":" + Convert.ToString((mm) + " PM");
+            }
+            else if (hh == 12)
+            {
+                time = Convert.ToString((hh)) + ":" + Convert.ToString((mm) + " PM");
+
+            }
+            else
+            {
+                time = Convert.ToString((hh)) + ":" + Convert.ToString((mm) + " AM");
+
+            }
+
+            trim =  trim.Remove(trim.Length - 5);
+
+            date = trim.Substring(trim.Length - 8);
+            date = date.Insert(2, "-");
+            date = date.Insert(5, "-");
+
+
+            trim = trim.Remove(trim.Length - 9);
+
+             symbol = trim;
+
+
+            if (choice == 1)
+            {
+                return symbol.ToUpper();
+            }
+            else if (choice == 2)
+            {
+                return date;
+            }
+            else if (choice == 3)
+            {
+                return time;
+            }
+            else
+            {
+                return null;
+            }
+   
+
+
+            
+
+        }
+
+        public static class LanguageUtils
+        {
+            /// <summary>
+            /// Runs an operation and ignores any Exceptions that occur.
+            /// Returns true or falls depending on whether catch was
+            /// triggered
+            /// </summary>
+            /// <param name="operation">lambda that performs an operation that might throw</param>
+            /// <returns></returns>
+            public static bool IgnoreErrors(Action operation)
+            {
+                if (operation == null)
+                    return false;
+                try
+                {
+                    operation.Invoke();
+                }
+                catch
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            /// <summary>
+            /// Runs an function that returns a value and ignores any Exceptions that occur.
+            /// Returns true or falls depending on whether catch was
+            /// triggered
+            /// </summary>
+            /// <param name="operation">parameterless lamda that returns a value of T</param>
+            /// <param name="defaultValue">Default value returned if operation fails</param>
+            public static T IgnoreErrors<T>(Func<T> operation, T defaultValue = default(T))
+            {
+                if (operation == null)
+                    return defaultValue;
+
+                T result;
+                try
+                {
+                    result = operation.Invoke();
+                }
+                catch
+                {
+                    result = defaultValue;
+                }
+
+                return result;
+            }
+        }
+
+       
+    }
+}
